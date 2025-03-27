359f729596d684d94794a3c8ab8d7dc95ad97d65
ViciousPear <karenova34@gmail.com>
2025-03-27 10:56:20 +0300

    add function for text recognition

--- src/model.py
+++ src/model.py
@@ -1,15 +1,9 @@
 from ultralytics import YOLO
-from PIL import Image
 import numpy
 import cv2
-import os
 import torch
 import os
-import psutil
-import pytesseract
-import re
 
-pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
 
 colors = [
     (255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0), (0, 255, 255),
@@ -18,63 +12,6 @@ colors = [
     (47, 79, 79), (47, 79, 47), (0, 206, 209), (148, 0, 211), (255, 20, 147)
 ]
 
-list_for_searching = []
-
-class QF():
-    __ID_QF = ''
-    __Current = ''
-    __Voltage = ''
-    __Current_Close = ''
-
-    def __init__(self, ID_QF, Current, Voltage, Current_Close):
-        self.__ID_QF = ID_QF
-        self.__Current = Current
-        self.__Voltage = Voltage
-        self.__Current_Close = Current_Close
-
-    @property
-    def ID_QF(self):
-        return self.__ID_QF
-
-    @property 
-    def Current(self):
-        return self.__Current
-    
-    @property
-    def Voltage(self):
-        return self.__Voltage
-    
-    @property
-    def Current_Close(self):
-        return self.__Current_Close
-    
-    @ID_QF.setter
-    def ID_QF(self, ID_QF):
-        self.__ID_QF = ID_QF
-
-    @Current.setter
-    def Current(self, Current):
-        self.__Current = Current
-
-    @Voltage.setter
-    def Voltage(self, Voltage):
-        self.__Voltage = Voltage
-
-    @Current_Close.setter
-    def Current_Close(self, Current_Close):
-        self.__Current_Close = Current_Close    
-  
-    def print_data(self):
-        print(self.__ID_QF, ':', self.__Current, self.__Voltage)
-
-
-class trans_TA:
-    __ta_name = ''
-    __ta_quantity = ''    
-
-def create_qf(ID_QF, Current, Voltage, Current_Close):
-    new_qf = QF(ID_QF, Current, Voltage, Current_Close)  # Создаем новый объект QF
-    return new_qf
 
 
 def learning_neuro():
@@ -98,159 +35,12 @@ def learning_neuro():
         workers=8
         #amp=False  # Временно отключено для теста
     )
-    
-def recognize_text_from_bbox(image, x1, y1, x2, y2):
-    #получение изображений по координатам
-    roi = image[y1:y2, x1:x2] 
 
-    # Увеличение на 200%
-    scale_percent = 200  
-    width = int(image.shape[1] * scale_percent / 100)
-    height = int(image.shape[0] * scale_percent / 100)
-    resized_image = cv2.resize(roi, (width, height), interpolation=cv2.INTER_CUBIC)
-   
-    # Преобразование в grayscale
-    gray_image = cv2.cvtColor(resized_image, cv2.COLOR_BGR2GRAY)
-   
-    # Бинарирование
-    _, binary_image = cv2.threshold(gray_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-
-    # Создание структурного элемента (ядро для морфологических операций)
-    kernel = numpy.ones((3, 3), numpy.uint8)
-
-    # Морфологическое расширение (dilation)
-    dilated_image = cv2.dilate(binary_image, kernel, iterations=1)
-
-    # Морфологическое сужение (erosion)
-    eroded_image = cv2.erode(dilated_image, kernel, iterations=1)
-
-    text = pytesseract.image_to_string(eroded_image, config='--oem 3 -c tessedit_char_whitelist="ABCFGQTmok0123456789/-зк')  # Запуск OCR
-
-    return text.strip()
-    
-
-def search_qf(text):
-    id_qf = r"\b\d*[OQ0]F[TG]?\d*\.?\d*\b"  # Идентификатор (например, QF19)
-    current_pattern = r"(\d+\s*-\s*\d+\s*A|\d+\s*A)"
-    voltage_pattern = r"(AC\d+/\d{3}|AC\w+)"
-    current_close_pattern = r"\d{3}(?=\D*kA)"
-    device_id = ''.join(re.findall(id_qf, text))
-    current_range = ''.join(re.findall(current_pattern, text)) 
-    current_voltage = ''.join(re.findall(voltage_pattern, text)) + 'В'\
-    if re.findall(voltage_pattern, text) else ''
-    current_close = ''.join(re.findall(current_close_pattern, text))
-    if (device_id != '' or current_range != '' or current_voltage != '' or current_close != ''):
-        new_qf = create_qf(device_id, current_range, current_voltage, current_close)
-        return new_qf
-    return
-    
-
-    
-def draw_rect(results, model, image):
-    thickness = 2
-    font_scale = 0.8
-    SCORE_THRESHOLD = 0.5
-    IOU_THRESHOLD = 0.4
-   
-    for result in results:
-        boxes = []
-        confidences = []
-        class_ids = []  # Храним классы для дальнейшего использования
-
-        # Сбор всех боксов и уверенности
-        for i, box in enumerate(result.boxes):
-            x1, y1, x2, y2 = map(int, box.xyxy[0])
-            conf = float(box.conf[0])
-
-            if conf >= SCORE_THRESHOLD:  # Отбрасываем слишком неуверенные боксы
-                boxes.append([x1, y1, x2 - x1, y2 - y1])  # (x, y, width, height)
-                confidences.append(conf)
-                class_ids.append(int(box.cls[0]))  # Сохраняем класс
-
-        # Проверяем, есть ли боксы вообще
-        if len(boxes) == 0:
-            return
-
-        # Фильтрация NMS
-        idxs = cv2.dnn.NMSBoxes(boxes, confidences, SCORE_THRESHOLD, IOU_THRESHOLD)
-
-        if len(idxs) > 0:
-            for i in idxs.flatten():  # Идём по индексам, оставшимся после NMS
-                x, y, w, h = boxes[i]  # Координаты бокса
-                cls = class_ids[i]  # Класс объекта
-               
-                color = [int(c) for c in colors[cls]]  # Цвет для класса
-
-                if cls == 8:  # Если класс объекта соответствует нужному (например, класс 8)
-                    # Вызовем функцию для распознавания текста в рамках bounding box
-                    text = recognize_text_from_bbox(image, x, y, x + w, y + h)
-                    print(f"Объект {model.names[cls]} ({conf:.2f}): {text}")
-                    if (search_qf(text) != None):
-                        qf = search_qf(text)
-                        # text_split = text.split('\n\n')
-                        # text_split = list(filter(lambda x: x != '', text_split))
-                        list_for_searching.append(qf)
-
-                # Отрисовка bounding box
-                cv2.rectangle(image, (x, y), (x + w, y + h), color, thickness)
-
-                # Подготовка текста
-                text = f"{model.names[cls]} {confidences[i]:.2f}"
-                (text_width, text_height) = cv2.getTextSize(
-                    text, cv2.FONT_HERSHEY_SIMPLEX, fontScale=font_scale, thickness=thickness
-                )[0]
-
-                # Координаты фона для текста
-                text_offset_x, text_offset_y = x, y - 5
-                box_coords = (
-                    (text_offset_x, text_offset_y - text_height - 2),
-                    (text_offset_x + text_width + 2, text_offset_y),
-                )
-
-                # Добавление фона (НЕ перезаписываем image!)
-                overlay = image.copy()
-                cv2.rectangle(overlay, box_coords[0], box_coords[1], color, -1)
-                cv2.addWeighted(overlay, 0.6, image, 0.4, 0, image)  # Модифицируем image напрямую
-
-                # Отображение текста
-                cv2.putText(
-                    image,
-                    text,
-                    (x, y - 5),
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    fontScale=font_scale,
-                    color=(255, 255, 255),
-                    thickness=thickness,
-                )
-
-
-def detect_one_image(image_path, model):
-
-    # Загрузить изображение
-    image = cv2.imread(image_path)
-    # Запустить модель YOLO на изображении
-   
-    results = model(image, conf=0.5, classes=[0,1,2,8])
-
-    draw_rect(results, model, image)
-
-    for qf in list_for_searching:
-        print(f"ID: {qf.ID_QF}, Ток: {qf.Current}, Напряжение: {qf.Voltage}, З. ток: {qf.Current_Close}")
-
-     # Развернуть окно на весь экран
-    window_name = "YOLOv8 Image Detection"
-    cv2.namedWindow(window_name, cv2.WND_PROP_FULLSCREEN)
-    cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-
-    # Показать изображение
-    cv2.imshow(window_name, image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
 
 def process_image(path, test_image):
 
  # Предобученная модель
-    model = YOLO('./runs/restudying_neuro_v5.61s/weights/best.pt')
+    model = YOLO('./runs/restudying_neuro_v5.71s/weights/best.pt')
      # Загрузка изображения
     image = cv2.imread(os.path.join(path, test_image))
     original_height, original_width = image.shape[:2]  # Сохраняем исходный размер изображения
@@ -347,10 +137,7 @@ def process_image(path, test_image):
 
 
 
-if __name__ == '__main__':
     learning_neuro()
-    # model = YOLO("./runs/restudying_neuro_v5.61s/weights/best.pt") 
-    # detect_one_image("tests/РУ_0,4_кВ_3200А_в_комплекте_с_шинным_мостами_ТП_49_ЭЛ_ЩИТ_17_06 (6)-05.png", model)
     
     # folder_path = "./tests"
     # img_list = []
@@ -363,5 +150,5 @@ if __name__ == '__main__':
     # for i in range(0, len(img_list)):
     #     process_image(folder_path, img_list[i])
 
-    #print("Физические ядра:", psutil.cpu_count(logical=False))
-    #print("Логические ядра:", psutil.cpu_count(logical=True))
+    # print("Физические ядра:", psutil.cpu_count(logical=False))
+    # print("Логические ядра:", psutil.cpu_count(logical=True))
---
+++ src/search_object.py
@@ -0,0 +1,128 @@
+from ultralytics import YOLO
+import cv2
+import search_text
+
+
+list_for_searching = []
+list_for_quantity = []
+
+colors = [
+    (255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0), (0, 255, 255),
+    (255, 0, 255), (192, 192, 192), (128, 128, 128), (128, 128, 0), (128, 128, 0),
+    (0, 128, 0), (128, 0, 128), (0, 128, 128), (0, 0, 128), (72, 61, 139),
+    (47, 79, 79), (47, 79, 47), (0, 206, 209), (148, 0, 211), (255, 20, 147)
+]
+
+def draw_rect(results, model, image):
+    thickness = 2
+    font_scale = 0.8
+    SCORE_THRESHOLD = 0.5
+    IOU_THRESHOLD = 0.4
+   
+    for result in results:
+        boxes = []
+        confidences = []
+        class_ids = []  # Храним классы для дальнейшего использования
+
+        # Сбор всех боксов и уверенности
+        for i, box in enumerate(result.boxes):
+            x1, y1, x2, y2 = map(int, box.xyxy[0])
+            conf = float(box.conf[0])
+
+            if conf >= SCORE_THRESHOLD:  # Отбрасываем слишком неуверенные боксы
+                boxes.append([x1, y1, x2 - x1, y2 - y1])  # (x, y, width, height)
+                confidences.append(conf)
+                class_ids.append(int(box.cls[0]))  # Сохраняем класс
+
+        # Проверяем, есть ли боксы вообще
+        if len(boxes) == 0:
+            return
+
+        # Фильтрация NMS
+        idxs = cv2.dnn.NMSBoxes(boxes, confidences, SCORE_THRESHOLD, IOU_THRESHOLD)
+
+        if len(idxs) > 0:
+            for i in idxs.flatten():  # Идём по индексам, оставшимся после NMS
+                x, y, w, h = boxes[i]  # Координаты бокса
+                cls = class_ids[i]  # Класс объекта
+               
+                color = [int(c) for c in colors[cls]]  # Цвет для класса
+
+                if cls == 8:  # Если класс объекта соответствует нужному (например, класс 8)
+                    # Вызовем функцию для распознавания текста в рамках bounding box
+                    text = search_text.recognize_text_from_bbox(image, x, y, x + w, y + h)
+                    print(f"Объект {model.names[cls]} ({conf:.2f}): {text}")
+                    if (search_text.search_qf(text) != None):
+                        qf_1 = search_text.search_qf(text)
+                        # text_split = text.split('\n\n')
+                        # text_split = list(filter(lambda x: x != '', text_split))
+                        list_for_searching.append(qf_1)
+                    if(search_text.search_ta(text) != None):
+                        ta_1 = search_text.search_ta(text)
+                        list_for_quantity.append(ta_1)
+
+                # Отрисовка bounding box
+                cv2.rectangle(image, (x, y), (x + w, y + h), color, thickness)
+
+                # Подготовка текста
+                text = f"{model.names[cls]} {confidences[i]:.2f}"
+                (text_width, text_height) = cv2.getTextSize(
+                    text, cv2.FONT_HERSHEY_SIMPLEX, fontScale=font_scale, thickness=thickness
+                )[0]
+
+                # Координаты фона для текста
+                text_offset_x, text_offset_y = x, y - 5
+                box_coords = (
+                    (text_offset_x, text_offset_y - text_height - 2),
+                    (text_offset_x + text_width + 2, text_offset_y),
+                )
+
+                # Добавление фона (НЕ перезаписываем image!)
+                overlay = image.copy()
+                cv2.rectangle(overlay, box_coords[0], box_coords[1], color, -1)
+                cv2.addWeighted(overlay, 0.6, image, 0.4, 0, image)  # Модифицируем image напрямую
+
+                # Отображение текста
+                cv2.putText(
+                    image,
+                    text,
+                    (x, y - 5),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    fontScale=font_scale,
+                    color=(255, 255, 255),
+                    thickness=thickness,
+                )
+
+
+def detect_one_image(image_path, model):
+
+    # Загрузить изображение
+    image = cv2.imread(image_path)
+    # Запустить модель YOLO на изображении
+
+    results = model(image, conf=0.5, classes=[0,1,2,8])
+
+    draw_rect(results, model, image)
+    
+    for qf_obj in list_for_searching:
+        print(f"ID: {qf_obj.ID_QF}, Ток: {qf_obj.Current}, Напряжение: {qf_obj.Voltage}, Откл. способность: {qf_obj.Current_Close}")
+
+    for ta_obj in list_for_quantity:
+        print(f"ID: {ta_obj.ta_name}, Количество: {ta_obj.ta_quantity}")
+
+     # Развернуть окно на весь экран
+    window_name = "YOLOv8 Image Detection"
+    cv2.namedWindow(window_name, cv2.WND_PROP_FULLSCREEN)
+    cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
+    # Показать изображение
+    cv2.imshow(window_name, image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+
+if __name__ == '__main__':
+
+    model = YOLO("./runs/restudying_neuro_v5.71s/weights/best.pt") 
+    detect_one_image("tests/youtohaveseethis.png", model)
